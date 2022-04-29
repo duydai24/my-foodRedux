14297c7763761bc4be6e63d18fb778141c33e8c0
duydai24 <nguyenduydai24@gmail.com>
2022-04-29 17:23:46 +0700

    fix button

--- src/lib/buttonOderNow/buttonOderNow.js
+++ src/lib/buttonOderNow/buttonOderNow.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function ButtonOderNow() {
   return (
-    <Link href="/Shop">
+    <Link href="/Shop" passHref={true}>
       <button className="bg-[#ff514e] rounded-3xl font-bold text-white py-2 px-3 uppercase border-2 border-[#ff514e] hover:bg-white hover:text-[#ff514e]">
         Order Now
       </button>
